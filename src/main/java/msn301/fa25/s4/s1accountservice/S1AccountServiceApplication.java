758b5543f62b1a3bde1ca4f5e9aefffad8b55094
sonnamnguyen <sonnamsonnam402@gmail.com>
2025-09-24 12:04:55 +0700

    feat: updateDBConfig

--- src/main/java/msn301/fa25/s4/s1accountservice/S1AccountServiceApplication.java
+++ src/main/java/msn301/fa25/s4/s1accountservice/S1AccountServiceApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class S1AccountServiceApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(S1AccountServiceApplication.class, args);
     }
 

570d38e9bc3c79b403502f4a5908c9f76a9a44a3
sonnamnguyen <sonnamsonnam402@gmail.com>
2025-09-25 09:34:46 +0700

    fix: CORS fix 2

--- src/main/java/msn301/fa25/s4/s1accountservice/config/OpenAIConfig.java
+++ src/main/java/msn301/fa25/s4/s1accountservice/config/OpenAIConfig.java
@@ -1,11 +1,24 @@
 package msn301.fa25.s4.s1accountservice.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "S1 Account Service API",
+                version = "1.0",
+                description = "API documentation for S1 Account Service"
+        ),
+        servers = {
+                @Server(url = "https://mssaccountservice-production.up.railway.app")
+        }
+)
 public class OpenAIConfig {
         @Bean
         public WebMvcConfigurer corsConfigurer() {
